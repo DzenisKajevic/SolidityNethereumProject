beea8e16c1df54395b267a1679216d35dcf36b0e
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2022-12-05 18:22:45 +0100

    Experimenting for a week

---
+++ VS Deployment/Nethereum Project/Nethereum Project/Class1.cs
@@ -0,0 +1,124 @@
+﻿using Nethereum.Contracts;
+using Nethereum.Hex.HexTypes;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nethereum_Project
+{
+    class ContractService
+    {
+        private readonly Web3 web3;
+        private readonly Contract contract;
+        private readonly Account account;
+
+        private static readonly HexBigInteger GAS_LIMIT = new HexBigInteger(4600000);
+
+        public ContractService(string provider, string contractAddress, string abi, string privateKey)
+        {
+            this.account = new Account(privateKey);
+            this.web3 = new Web3(account, provider);
+            this.contract = web3.Eth.GetContract(abi, contractAddress);
+        }
+
+        //        public async Task<Nethereum.RPC.Eth.DTOs.TransactionReceipt> Multiply(int val)
+        public async Task<int> Multiply(int val)
+
+        {
+            /* // params for SendTransactionAsync
+            The value parameter refers to the amount of Ether you want to send to the contract with that transaction.
+
+            When sending a transaction there are common parameters:
+
+            To (the address where you sending the transaction, in this case, the contract address which is automatically set)
+            From (the address from)
+            Gas (the total amount of gas you want to spend, or gas limit)
+            Gas Price (gas price)
+            Value (the amount of ether (in Wei) you want to send, this can be to an account or a contract, in your scenario, you will be sending it to a contract. 
+            Your function in solidity should be able to access it using msg.value)
+            Data (in your scenario, this is the function and parameters encoded)
+             */
+            try
+            {
+                var multiplyFunction = contract.GetFunction("multiplyWithoutEvent");
+                var result = await multiplyFunction.CallAsync<int>(val);
+                //var receipt = await multiplyFunction.SendTransactionAndWaitForReceiptAsync(account.Address, GAS_LIMIT, new HexBigInteger(0), null, val);
+                //Console.WriteLine(receipt.BlockHash);
+                //var txHash = multiplyFunction.SendTransactionAsync(account.Address, GAS_LIMIT, new HexBigInteger(0), val);
+                return result;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return -1;
+        }
+
+        public async Task MultipliedEvent()
+        {
+            var multiplyFunction = contract.GetFunction("multiplyWithEvent");
+            var multiplyEvent = contract.GetEvent("multiplied");
+
+            var filterAll = await multiplyEvent.CreateFilterAsync();
+            var filter5 = await multiplyEvent.CreateFilterAsync(7);
+
+            //var receipt = await multiplyFunction.SendTransactionAndWaitForReceiptAsync(account.Address, GAS_LIMIT, GAS_LIMIT, null, 4);
+            //var receipt5 = await multiplyFunction.SendTransactionAndWaitForReceiptAsync(account.Address, GAS_LIMIT, GAS_LIMIT, null, 5);
+            
+            // comment 3 lines below, uncomment 3 above
+
+            var transactionHash = await multiplyFunction.SendTransactionAsync(account.Address, GAS_LIMIT, GAS_LIMIT, 4); // await multiplyFunction.SendTransactionAsync(account.Address, 4);
+            transactionHash = await multiplyFunction.SendTransactionAsync(account.Address, GAS_LIMIT, GAS_LIMIT,  5);
+            var receipt = await web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(transactionHash);
+            Console.WriteLine(receipt);
+
+            // use the generic method to be able to decode it (<MultipliedEvent>)
+            // if we have complex output, we can create "function output" or a DTO
+            var log = await multiplyEvent.GetFilterChangesAsync<MultipliedEventClass>(filterAll);
+            var log5 = await multiplyEvent.GetFilterChangesAsync<MultipliedEventClass>(filter5);
+
+            Console.WriteLine("log count: " + log.Count);
+            Console.WriteLine("log5 count: " + log5.Count);
+            Console.WriteLine("log5 first parameter (input): " + log5[0].Event.multiplicationInput);
+            Console.WriteLine("log5 result: " + log5[0].Event.result);
+            Console.WriteLine("complete log" + log);
+        }
+
+        public async Task functionDTOs()
+        {
+
+        }
+
+        // class that can be decoded as the event output
+        // event multiplied(int256 indexed val, address indexed sender, int256 result);
+        public class MultipliedEventClass
+        {
+            public int multiplicationInput { get; set; }
+
+            public string sender { get; set; }
+
+            public int result { get; set; }
+
+        }
+
+        static async Task Main(string[] args)
+        {
+            var provider = "https://goerli.infura.io/v3/890a7f3aaf0d4b8eae37992a8e12a982";
+            var contractAddress = "0x3bf85A914dD259440BB2CcB589127bfF21a6Ef2B";
+            var abi = "[{'inputs':[{'internalType':'int256','name':'multiplier','type':'int256'}],'stateMutability':'nonpayable','type':'constructor'},{'anonymous':false,'inputs':[{'indexed':true,'internalType':'int256','name':'val','type':'int256'},{'indexed':true,'internalType':'address','name':'sender','type':'address'},{'indexed':false,'internalType':'int256','name':'result','type':'int256'}],'name':'multiplied','type':'event'},{'inputs':[{'internalType':'int256','name':'val','type':'int256'}],'name':'multiplyWithEvent','outputs':[{'internalType':'int256','name':'result','type':'int256'}],'stateMutability':'payable','type':'function'},{'inputs':[{'internalType':'int256','name':'val','type':'int256'}],'name':'multiplyWithoutEvent','outputs':[{'internalType':'int256','name':'result','type':'int256'}],'stateMutability':'view','type':'function'}]";
+            var privateKey = "d56fafc4fec3addbcf487a807d76be569942d16ef7e5bd3dc05196e8844e0626";
+            
+
+            ContractService contractService = new ContractService(provider, contractAddress, abi, privateKey);
+            Console.WriteLine($"Multiplied value: {await contractService.Multiply(4)}");
+
+            Console.WriteLine("Entering event territory");
+            await contractService.MultipliedEvent();
+            //var idk = await contractService.Multiply(4);
+            //Console.WriteLine(idk);
+        }
+    }
+}
---
+++ VS Deployment/Nethereum Project/Nethereum Project/Program.cs
@@ -0,0 +1,84 @@
+﻿using Nethereum.Hex.HexTypes;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+using Xunit;
+using Nethereum.RPC.TransactionManagers;
+
+namespace Nethereum_Project
+{
+    public class TestClass
+    {
+        private static readonly HexBigInteger GAS = new HexBigInteger(4600000);
+//        static async Task Main(string[] args) {
+//            await ShouldBeAbleToDeployAContract();
+//        }
+
+        [Fact]
+        static async Task ShouldBeAbleToDeployAContract()
+        {
+            var senderAddress = "0x12890d2cce102216644c59daE5baed380d84830c";
+            var password = "password";
+            var abi = @"[{'inputs':[{'internalType':'int256','name':'multiplier','type':'int256'}],'stateMutability':'nonpayable','type':'constructor'},{'anonymous':false,'inputs':[{'indexed':true,'internalType':'int256','name':'val','type':'int256'},{'indexed':true,'internalType':'address','name':'sender','type':'address'},{'indexed':false,'internalType':'int256','name':'result','type':'int256'}],'name':'multiplied','type':'event'},{'inputs':[{'internalType':'int256','name':'val','type':'int256'}],'name':'multiply','outputs':[{'internalType':'int256','name':'result','type':'int256'}],'stateMutability':'nonpayable','type':'function'}]";
+            var byteCode = "0x608060405234801561001057600080fd5b506040516101a83803806101a883398101604081905261002f91610037565b600055610050565b60006020828403121561004957600080fd5b5051919050565b6101498061005f6000396000f3fe608060405234801561001057600080fd5b506004361061002b5760003560e01c80631df4f14414610030575b600080fd5b61004361003e3660046100ae565b610055565b60405190815260200160405180910390f35b6000805461006390836100dd565b9050336001600160a01b0316827fe4b5609d42bdc604de81c0e89871802d367c2ce35a352c14630cc79e63a3facc836040516100a191815260200190565b60405180910390a3919050565b6000602082840312156100c057600080fd5b5035919050565b634e487b7160e01b600052601160045260246000fd5b80820260008212600160ff1b841416156100f9576100f96100c7565b818105831482151761010d5761010d6100c7565b9291505056fea2646970667358221220482cc13c675efa428e1e7c17898fc8edd7673cfceb15193a2ea053ee271ef95f64736f6c63430008110033";
+
+            var multiplier = 7;
+
+            var web3Geth = new Nethereum.Geth.Web3Geth("http://127.0.0.1:8545");
+            var web3 = new Nethereum.Web3.Web3(); //defalut RPC 8545
+            var unlockAccountResult = await web3Geth.Personal.UnlockAccount.SendRequestAsync
+                (senderAddress, password, 120);
+            Assert.True(unlockAccountResult);
+
+            var transactionManager = web3.TransactionManager;
+
+            /*
+            web3Geth.TransactionManager.DefaultGas = transactionManager.DefaultGas;
+            web3Geth.TransactionManager.DefaultGasPrice = transactionManager.DefaultGasPrice;
+*/
+            // returned before the transaction is mined
+            //, new HexBigInteger(transactionManager.DefaultGas)
+            var transactionHash = await web3Geth.Eth.DeployContract.SendRequestAsync(abi, byteCode, senderAddress, GAS, multiplier);
+
+            // if we post this on a public chain, we don't need to mine it, but since this is on a private chain, we do need to do it
+            var mineResult = await web3Geth.Miner.Start.SendRequestAsync(6);
+
+            Assert.True(mineResult);
+
+            // receipt of the transaction that will contain the contract adddress
+            // receipt contains various details about the transaction, including whether it was mined successfully or not,
+            // how much the Gas Price was, etc.
+            var receipt = await web3Geth.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(transactionHash);
+
+            // if the transaction hasn't been mined yet, the receipt will be null
+            while(receipt == null)
+            {
+                Thread.Sleep(5000);
+                receipt = await web3Geth.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(transactionHash);
+            }
+
+            Console.WriteLine(receipt);
+            var contractAddress = receipt.ContractAddress;
+
+            // creates contract wrapper from the contract address & abi
+            var contract = web3.Eth.GetContract(abi, contractAddress);
+
+            var multiplyFunction = contract.GetFunction("multiply");
+
+            // call vs transcation
+            // call is execution of a function within a blockchain without any verification
+            // call and get the data, it doesn't require any kind of consensus from the chain
+            // transaction -> submits result, requires consensus from the chain, doesn't return anything
+
+            // events -> Put information on the log (does not need to be paid)
+            // Events allow the convenient usage of the EVM logging facilities, which in turn can be used
+            // to “call” JavaScript callbacks in the user interface of a dapp, which listen for these events.
+            // log information can be retrieved using an ethereum client or something like nethereum 
+
+
+            var result = await multiplyFunction.CallAsync<int>(7);
+
+            Assert.Equal(49, result);
+        }
+    }
+}
