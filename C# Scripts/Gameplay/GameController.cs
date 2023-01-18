6b4e396780d1a066cbb20c4bb1920afc48c2f1ee
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2023-01-18 22:38:45 +0100

    Forgot to push GameController update :)

--- C# Scripts/Gameplay/GameController.cs
+++ C# Scripts/Gameplay/GameController.cs
@@ -27,26 +27,10 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        /*
         Debug.Log("Restarted: " + restarted.Value);
         Debug.Log("PrivateKey: " + loggedInPlayerSO.PrivateKey);
         Debug.Log("PublicKey: " + loggedInPlayerSO.PublicKey);
-        /*
-        if (restarted.Value)
-        {
-            Debug.Log("Restarted: " + restarted.Value);
-            restarted.Value = false;
-            camera.transform.position = new Vector3(0, 0, -25);
-            StartGame();
-        }
-        //gameOverCountdown.gameObject.SetActive(false);
-        else
-        {
-            Debug.Log("Restarted: " + restarted.Value);
-            Time.timeScale = 0;
-
-            StartCoroutine(loadSkinScript.loadSkin());
-            
-        }
         */
         Time.timeScale = 0;
 
@@ -55,21 +39,7 @@ public class GameController : MonoBehaviour
 
     private void Update()
     {
-        // was planned as a timer for automatically restarting the game, but that idea was scrapped (for now)
-        /*
-        if (player.isDead)
-        {
-            gameOverCountdown.gameObject.SetActive(true);
-            countTimer -= Time.unscaledDeltaTime;
-        }
 
-        gameOverCountdown.text = "Restarting in " + (countTimer).ToString("0");
-
-        if (countTimer < 0)
-        {
-            RestartGame();
-        }
-        */
     }
 
     public void StartGame()
@@ -85,7 +55,6 @@ public class GameController : MonoBehaviour
         Time.timeScale = 0;
         restartButton.SetActive(true);
         mainMenuButton.SetActive(true);
-        //EditorSceneManager.LoadScene("Flappy");
     }
 
     public void RestartGame()
@@ -101,9 +70,7 @@ public class GameController : MonoBehaviour
     public void ReturnToMainMenu()
     {
         restarted.Value = false;
-        loggedInPlayerSO.PrivateKey = null;
-        loggedInPlayerSO.PublicKey = null;
-        loggedInPlayerSO.SkinList = null;
+        loggedInPlayerSO.resetValues();
 #if UNITY_EDITOR
             EditorSceneManager.LoadScene("Main Menu");
 #else
